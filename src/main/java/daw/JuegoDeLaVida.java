a1225561dd61b09b22857a8b6341356305b48da9
Ignacio <idurmes337@g.educaand.es>
2025-03-18 09:55:40 +0100

    Primer commmit

---
+++ src/main/java/daw/JuegoDeLaVida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author ignacio
+ */
+public class JuegoDeLaVida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
